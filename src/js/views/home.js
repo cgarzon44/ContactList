bdf4cf9f54f1a956650c26d031428442c06c5648
cgarzon44 <cgarzon44@gmail.com>
2021-11-21 20:18:57 +0000

    push1

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -3,10 +3,10 @@ import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
 
 export const Home = () => (
-	<div className="text-center mt-5">
+	<form className="text-center mt-5">
 		<label>Contact Name:</label>
 		<input type="text" />
 		<label>Email:</label>
 		<input type="text" />
-	</div>
+	</form>
 );
